dea7c260950053465f3a3b9a8aa0fc198d1822f3
Angel Vasilev <angavasilev@gmail.com>
2022-08-11 11:51:23 +0300

    Add initial template

---
+++ Server/AirsoftShop.Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirsoftShop.WebApi.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return null;
+    }
+
+    public IActionResult Privacy()
+    {
+        return null;
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return null;
+    }
+}
