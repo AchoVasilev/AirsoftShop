590a4ef693f20d296981c004d0de33cfda99dd02
Angel Vasilev <angavasilev@gmail.com>
2022-08-29 13:40:17 +0300

    Add routes to order routing module

--- Client/AirsoftClient/src/app/features/order/order-routing.module.ts
+++ Client/AirsoftClient/src/app/features/order/order-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/infrastructure/guards/auth-guard';
+import { ClientGuard } from 'src/app/infrastructure/guards/client.guard';
+import { ClientComponent } from './client/client.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'client',
+        component: ClientComponent,
+        pathMatch: 'full',
+        canActivate: [ClientGuard]
+      },
+      {
+        path: 'client/:id',
+        component: DetailsComponent,
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
