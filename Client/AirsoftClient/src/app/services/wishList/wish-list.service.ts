c7998a7f40886e41d428b1cc43c12d6bcc5cef8e
Angel Vasilev <angavasilev@gmail.com>
2022-09-03 13:56:06 +0300

    Add clientside wish list component

---
+++ Client/AirsoftClient/src/app/services/wishList/wish-list.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WishListModel } from 'src/app/models/wishList/WishListModel';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WishListService {
+  private apiUrl: string = environment.apiUrl;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getItems(): Observable<WishListModel[]> {
+    return this.httpClient.get<WishListModel[]>(`${this.apiUrl}/wishlists`);
+  }
+
+  removeItem(id: string): Observable<any> {
+    return this.httpClient.delete(`${this.apiUrl}/wishlists`, {
+      body: id
+    });
+  }
+
+  removeItems(ids: string[]): Observable<any> {
+    return this.httpClient.delete(`${this.apiUrl}/wishlists/bulkRemove`, {
+      body: ids
+    });
+  }
+}
